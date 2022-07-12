1cfbfb0d29ff42312b5ba7adee683989e73b8824
MrLoh <flojoto@hotmail.com>
2022-07-12 09:39:47 -0500

    build ssr rendered app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { MouseEvent as _MouseEvent, useState } from 'react';
 type MouseEvent<E = Element> = _MouseEvent<E> & { target: E };
 type Route = { path: string; render: () => JSX.Element; label: string };
 
-const App = () => {
-  const [currentPath, setCurrentPath] = useState<string>(window.location.pathname);
+export const routes: Route[] = [
+  { path: '/', render: () => <h1>Home</h1>, label: 'Home' },
+  { path: '/about', render: () => <h1>About</h1>, label: 'About' },
+];
+
+export default function App({ startPath = window.location.pathname }: { startPath?: string }) {
+  const [currentPath, setCurrentPath] = useState<string>(startPath);
 
   const handleRouteClick = (e: MouseEvent<HTMLAnchorElement>): void => {
     e.preventDefault();
@@ -12,14 +17,10 @@ const App = () => {
     window.history.pushState({}, '', path);
   };
 
-  const routes: Route[] = [
-    { path: '/', render: () => <h1>Home</h1>, label: 'Home' },
-    { path: '/about', render: () => <h1>About</h1>, label: 'About' },
-  ];
   const currentRoute: Route | undefined = routes.find(({ path }) => path === currentPath);
 
   return (
-    <div>
+    <>
       <nav>
         {routes.map(({ path, label }) => (
           <a key={path} href={path} onClick={handleRouteClick}>
@@ -28,8 +29,6 @@ const App = () => {
         ))}
       </nav>
       <main>{currentRoute ? currentRoute.render() : <h1>404</h1>}</main>
-    </div>
+    </>
   );
-};
-
-export default App;
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify';
 import staticPlugin from '@fastify/static';
 import { readFile } from 'fs/promises';
 import path from 'path';
+import React, { ReactElement } from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App, { routes } from './App';
 
 const server = fastify({ logger: true });
 
@@ -10,8 +14,15 @@ server.register(staticPlugin, {
   prefix: '/static',
 });
 
-server.get('/*', async (req, res) => {
-  const html = await readFile(path.join(__dirname, '../build/index.html'), 'utf8');
+server.get<{ Params: { path: string } }>('/:path', async (req, res) => {
+  const startPath: string = '/' + req.params.path;
+  if (!routes.find(({ path }) => path === startPath)) res.status(404);
+
+  const htmlWrapper: string = await readFile(path.join(__dirname, '../build/index.html'), 'utf8');
+  const appElement: ReactElement = React.createElement(App, { startPath });
+  const appHtml: string = ReactDOMServer.renderToString(appElement);
+  const html: string = htmlWrapper.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
+
   res.type('text/html').send(html);
 });
 
